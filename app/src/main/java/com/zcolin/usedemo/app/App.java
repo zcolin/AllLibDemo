984a50a84fca9d7370f1c19e88bdfa6550cdae9c
colin <wanglin2046@126.com>
2017-02-14 10:40:33 +0800

    # 视频录制操作摄像头的地方增加异常处理

--- app/src/main/java/com/zcolin/usedemo/app/App.java
+++ app/src/main/java/com/zcolin/usedemo/app/App.java
@@ -32,6 +32,19 @@ public class App extends BaseApp {
     @Override
     public void onCreate() {
         super.onCreate();
+        //if (LeakCanary.isInAnalyzerProcess(this)) {
+        //            // This process is dedicated to LeakCanary for heap analysis.
+        //            // You should not init your app in this process.
+        //            return;
+        //        }
+        //        LeakCanary.install(this);
+        // Normal app init code...
+
+        //facebook调试工具
+        //if (BuildConfig.DEBUG) {
+        //    Stetho.initializeWithDefaults(this);
+        //}
+        
         APP_CONTEXT = BaseApp.APP_CONTEXT;
         initHttpOptions();
     }
@@ -39,20 +52,22 @@ public class App extends BaseApp {
     private void initHttpOptions() {
         HttpsUtils.SSLParams sslParams = HttpsUtils.getSslSocketFactory(null, null, null);
         CookieJarImpl cookieJar1 = new CookieJarImpl(new MemoryCookieStore());
-        OkHttpClient okHttpClient = new OkHttpClient.Builder()
-                .connectTimeout(10000L, TimeUnit.MILLISECONDS)
-                .readTimeout(10000L, TimeUnit.MILLISECONDS)
-                .cookieJar(cookieJar1)
-                .hostnameVerifier(new HostnameVerifier() {
-                    @Override
-                    public boolean verify(String hostname, SSLSession session) {
-                        return true;
-                    }
-                })
-                .sslSocketFactory(sslParams.sSLSocketFactory, sslParams.trustManager)
-                .build();
+        OkHttpClient.Builder builder = new OkHttpClient.Builder();
+        //facebook调试工具
+        //        if (BuildConfig.DEBUG) {
+        //            builder.addNetworkInterceptor(new StethoInterceptor());
+        //        }
+        OkHttpClient okHttpClient = builder.connectTimeout(10000L, TimeUnit.MILLISECONDS)
+                                           .readTimeout(10000L, TimeUnit.MILLISECONDS)
+                                           .cookieJar(cookieJar1)
+                                           .hostnameVerifier(new HostnameVerifier() {
+                                               @Override
+                                               public boolean verify(String hostname, SSLSession session) {
+                                                   return true;
+                                               }
+                                           })
+                                           .sslSocketFactory(sslParams.sSLSocketFactory, sslParams.trustManager)
+                                           .build();
         OkHttpUtils.initClient(okHttpClient);
     }
-
-
 }
